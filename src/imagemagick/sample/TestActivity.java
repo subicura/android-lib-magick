b9a75840c3f1581551ff8681ed42422d10cd778e
subicura <subicura@subicura.com>
2013-05-12 11:54:49 +0900

    draw text sample

--- src/imagemagick/sample/TestActivity.java
+++ src/imagemagick/sample/TestActivity.java
@@ -1,15 +1,18 @@
 package imagemagick.sample;
 
+import helper.TempStorageHelper;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import magick.CompositeOperator;
+import magick.DrawInfo;
+import magick.GravityType;
 import magick.ImageInfo;
 import magick.MagickException;
 import magick.MagickImage;
-
-import helper.TempStorageHelper;
+import magick.PixelPacket;
 import teste.ndk.R;
 import android.app.Activity;
 import android.content.Intent;
@@ -28,6 +31,7 @@ public class TestActivity extends Activity {
         try {
 			tempStorageHelper.copyAsset(this, "bg.jpg");
 			tempStorageHelper.copyAsset(this, "android.png");
+			tempStorageHelper.copyAsset(this, "OpenSans-Semibold.ttf");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -36,9 +40,23 @@ public class TestActivity extends Activity {
         	ImageInfo canvasInfo = new ImageInfo(tempStorageHelper.getTempFilePath("bg.jpg"));
         	MagickImage canvasImage = new MagickImage(canvasInfo);
         	
+        	// add image
         	MagickImage image = new MagickImage(new ImageInfo(tempStorageHelper.getTempFilePath("android.png")));
         	canvasImage.compositeImage(CompositeOperator.AtopCompositeOp, image, 266, 538);
         	
+        	// add text
+			ImageInfo textImage = new ImageInfo();
+			DrawInfo aInfo = new DrawInfo(textImage);
+			aInfo.setFill(PixelPacket.queryColorDatabase("black"));
+			aInfo.setOpacity(0);
+			aInfo.setPointsize(50);
+			aInfo.setFont(tempStorageHelper.getTempFilePath("OpenSans-Semibold.ttf"));
+			aInfo.setTextAntialias(true);
+			aInfo.setText("This is Text!!");
+		    aInfo.setGeometry("730x70+0+900");
+		    aInfo.setGravity(GravityType.CenterGravity);
+		    canvasImage.annotateImage(aInfo);
+        			    
         	try {
     			byte blob[] = canvasImage.imageToBlob(canvasInfo);
     			FileOutputStream fos = new FileOutputStream(new File(Environment.getExternalStorageDirectory() + "/Download/test.jpg"));
